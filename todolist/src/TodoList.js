7733cd376c01165f0bb35fa31bafbc368db31104
1521350289@qq.com <1521350289@qq.com>
2018-12-12 22:42:30 +0800

    .

--- todolist/src/TodoList.js
+++ todolist/src/TodoList.js
@@ -9,14 +9,18 @@ class TodoList extends Component {
 	constructor(props) {
 		super(props);
 		this.state = store.getState();
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleStoreChange = this.handleStoreChange.bind(this);
+		this.handleBtnClick = this.handleBtnClick.bind(this);
+		store.subscribe(this.handleStoreChange);
 	}
 
 	render() {
 		return (
 			<div style={{marginTop: '10px',marginLeft: '10px'}}>
 				<div>
-					<Input value={this.state.inputValue} placeholder="todo info" style={{width: '300px',marginRight: '10px'}} />
-					<Button type="primary">提交</Button>
+					<Input onChange={this.handleInputChange} value={this.state.inputValue} placeholder="todo info" style={{width: '300px',marginRight: '10px'}} />
+					<Button type="primary" onClick={this.handleBtnClick}>提交</Button>
 				</div>
 				<List
 				  style={{marginTop: '10px',width: '300px'}}
@@ -26,6 +30,26 @@ class TodoList extends Component {
 			    />
 			</div>
 		)
+	}
+
+	handleInputChange(e) {
+		const action = {
+			type: 'change_input_value',
+			value: e.target.value
+		}
+		store.dispatch(action);
+		console.log(e.target.value);
+	}
+
+	handleStoreChange() {
+		this.setState(store.getState());
+	}
+
+	handleBtnClick() {
+		const action = {
+			type: 'add_todo_item'
+		};
+		store.dispatch(action);
 	}
 }
 
--- todolist/src/store/reducer.js
+++ todolist/src/store/reducer.js
@@ -3,6 +3,18 @@ const defaultState = {
 	list: []
 }
 
+//reducer 可以接收state 但不能修改
 export default (state = defaultState, action) => {
+	if (action.type === 'change_input_value') {
+		const newState = JSON.parse(JSON.stringify(state));
+		newState.inputValue = action.value;
+		return newState;
+	}
+	if (action.type === 'add_todo_item') {
+		const newState =JSON.parse(JSON.stringify(state));
+		newState.list.push(newState.inputValue);
+		newState.inputValue = '';
+		return newState;
+	}
 	return state;
 }
